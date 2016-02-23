42a139ee120574be9e222c72669299c2f10a259a
Conor Maguire <conormag94@gmail.com>
2016-02-23 23:57:40 +0000

    Progress

--- proxy.py
+++ proxy.py
@@ -1,5 +1,29 @@
 import sys
 import socket
+import threading
+
+def website_blocked(website):
+    with open ('blocklist.txt', 'r') as blocklist:
+        for line in blocklist:
+            print(line)
+            if website in line:
+                return True
+    return False
+
+def proxy_thread(clientsocket, address):
+    print("Started a thread")
+    ip, port = address
+    print("Client IP:", ip, "Port:", port)
+    request = clientsocket.recv(4096).decode()
+    print(request)
+
+    website = 'thing'
+    response = 'This is passing through the proxy'
+    if website_blocked(website):
+        response = website + ' is blocked by the proxy'
+
+    clientsocket.send(response.encode())
+    clientsocket.close()
 
 def main():
     print(sys.argv)
@@ -10,10 +34,12 @@ def main():
 
     while True:
         (clientsocket, address) = s.accept()
-        ip, port = address
-        print("Client IP:", ip, "Port:", port)
-
+        threading.Thread(target=proxy_thread, args=(clientsocket, address)).start()
     s.close()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Stopping server...")
+        sys.exit(1)
